322ebd6a843b7d2c75ec856bc5c014098e65af7c
Theino <todd.a.heino@gmail.com>
2016-08-24 14:39:37 -0400

    Object Oriented Working
    Got object oriented finally working. Its also using mouseX and mouseY

--- practiceSketch1/practiceSketch1.js
+++ practiceSketch1/practiceSketch1.js
@@ -1,27 +1,23 @@
-var ball;
+var ballA;
 
 function setup() {
   createCanvas(640,360);
   stroke(255);
   fill(255);
+  background(0);
+  ballA = new Ball();
 }
 
 function draw() {
   if(!mouseIsPressed) {
     background(0);
   }
-  createBall(mouseX, mouseY);
+  ballA.display();
 }
 
 function Ball() {
   this.diameter = 50;
-  this.x = 50;
-  this.y = 50;
   this.display = function () {
-    ellipse(this.x, this.y, this.diameter, this.diameter);
+    ellipse(mouseX, mouseY, this.diameter, this.diameter);
   }
-}
-
-function createBall(xLoc, yLoc) {
-  ellipse(xLoc, yLoc, 50, 50);
-}
+};
