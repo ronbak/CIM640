29c7ae8fdb019d15469e132b21e67e0d374a73a3
Theino <todd.a.heino@gmail.com>
2016-08-31 13:24:13 -0400

    Removed All CSS Junk and added initial Pong
    Key pressing working for paddles, ball moving around and bouncing off
    sides.
    
    Ball not yet interacting with paddles

--- practiceSketch1/practiceSketch1.js
+++ practiceSketch1/practiceSketch1.js
@@ -1,6 +1,7 @@
 var ballA;
 var canvasWidth = 640;
 var canvasHeight = 360;
+var paddleDistanceFromSide = 60;
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
@@ -8,15 +9,64 @@ function setup() {
   fill(255);
   background(0);
   ballA = new Ball();
+  paddleA = new Paddle(paddleDistanceFromSide);
+  paddleB = new Paddle(canvasWidth - paddleDistanceFromSide);
 }
 
 function draw() {
   background(0);
   ballA.display();
+  if(keyIsDown(80)) // p
+  {
+    paddleB.moveUp();
+  }
+  if(keyIsDown(76)) // l
+  {
+    paddleB.moveDown();
+  }
+  if(keyIsDown(81)) // q
+  {
+    paddleA.moveUp();
+  }
+  if(keyIsDown(65)) // a
+  {
+    paddleA.moveDown();
+  }
+  paddleA.display();
+  paddleB.display();
+}
+
+
+function Paddle(xPos) {
+    this.xPos = xPos;
+    this.yPos = canvasHeight/2.0;
+    this.height = 80;
+    this.width = 20;
+    this.highPoint = this.height/2.0;
+    this.lowPoint = canvasHeight - this.height/2.0;
+    this.display = function () {
+        rect(this.xPos - this.width/2.0, this.yPos - this.height/2.0, this.width, this.height);
+    }
+    this.moveUp = function () {
+        if(this.yPos >= this.highPoint)
+        {
+            this.yPos--;
+        }
+    }
+    this.moveDown = function () {
+        if(this.yPos <= this.lowPoint)
+        {
+            this.yPos++;
+        }
+    }
 }
 
 function Ball() {
   this.diameter = 50;
+  this.highPoint = this.diameter/2.0;
+  this.lowPoint = canvasHeight - this.diameter/2.0;
+  this.leftMostPoint = this.diameter/2.0;
+  this.rightMostPoint = canvasWidth - this.diameter/2.0;
   this.prevXLoc = this.diameter/2.0;
   this.prevYLoc = canvasHeight - this.diameter/2.0;
   this.xLoc = 0;
@@ -26,9 +76,27 @@ function Ball() {
   this.display = function () {
     this.moveBall();
     ellipse(this.xLoc, this.yLoc, this.diameter, this.diameter);
+    this.prevXLoc = this.xLoc;
+    this.prevYLoc = this.yLoc;
   }
   this.moveBall = function () {
-    this.xLoc = this.prevXLoc;
-    this.yLoc = this.prevYLoc;
+    if(this.xLoc > this.rightMostPoint)
+    {
+        this.xSpeed = -this.xSpeed;
+    }
+    if(this.xLoc < this.leftMostPoint)
+    {
+        this.xSpeed = -this.xSpeed;
+    }
+    if(this.yLoc > this.lowPoint)
+    {
+        this.ySpeed = -this.ySpeed;
+    }
+    if(this.yLoc < this.highPoint)
+    {
+        this.ySpeed = -this.ySpeed;
+    }
+    this.xLoc = this.prevXLoc + this.xSpeed;
+    this.yLoc = this.prevYLoc + this.ySpeed;
   }
 };
